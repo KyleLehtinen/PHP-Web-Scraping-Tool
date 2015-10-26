951fc079b2af1c47c1766420fdcd7cf1d6b78e81
Kyle Lehtinen <kylelehtinen@gmail.com>
2015-10-25 17:28:54 -0700

    Added ability to catch NSFW flagged memes and adjusted logic to handle it

--- index.php
+++ index.php
@@ -13,33 +13,45 @@ class Mog {
 	public $srcUrl;
 	public $rating;
 	public $rateBias;
+	public $active;
 
-	public function __construct($name, $imgUrl, $srcViews, $srcFaves, $srcUrl) {
+	public function __construct($name, $imgUrl, $srcViews, $srcFaves, $srcUrl, $active) {
 		$this->name = $name;
 		// $this->srcOrigin = $srcOrigin;
 		$this->imgUrl = $imgUrl;
 		// $this->localPath = $localPath;
 		$this->srcViews = $srcViews;
 		$this->srcFaves = $srcFaves;
-		$this->rating = ($srcViews * $srcFaves);
+		$this->rating = ($srcViews + $srcFaves);
 		$this->srcUrl = $srcUrl;
+		$this->active = $active;
 	}
 
-	public function save() {
+	public function saveToDB() {
 		try {
 			$db = new PDO('mysql:host=localhost;dbname=MemeSlamDev;charset=utf8','root','');
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$query = $db->prepare("insert into MogMaster (name, img_url, src_url, rating) 
-				values (:name, :imgUrl, :srcUrl, :rating)");
-			$query->execute(['name'=>$this->name, 'imgUrl'=>$this->imgUrl, 'srcUrl'=>$this->srcUrl, 'rating'=>$this->rating]);
+			$query = $db->prepare("insert into MogMaster (name, img_url, src_url, rating, active) 
+				values (:name, :imgUrl, :srcUrl, :rating, :active)");
+			$query->execute(['name'=>$this->name, 'imgUrl'=>$this->imgUrl, 'srcUrl'=>$this->srcUrl, 'rating'=>$this->rating, 'active'=>$this->active]);
 		} catch (PDOException $e) {
 			die($e->getMessage());	
 		}
 	}
 
+	public function saveToCSV($filename) {
+		$line = [];
+		$line[] = $this->name;
+		$line[] = $this->srcUrl;
+		$line[] = $this->rating;
+		$line[] = $this->active;
+		
+		$csv = fopen($filename, 'a') or die("Unable to open the file!");
+		fputcsv($csv, $line);
+		fclose($csv);
+	}
 }
 
-//Executed Code
 mainExecute();
 
 /*********************************
@@ -47,8 +59,18 @@ Functions for work
 **********************************/
 //Main Logic Function
 function mainExecute() {
+
+	//support variables for logic
+	$matches = [];
+	$alt_dom = '';
+	$i = 1;
+	$memeIDCounter = 1;
+	$page_count = 2;
+	$csvFileName = 'mogmaster.csv';
+	$toCSV = true;
+	
 	//array for offline pages testing
-	$offlinePages = ['staticpages/Slender Man _ Know Your Meme.html','staticpages/Forever Alone _ Know Your Meme.html', 'staticpages/Zerg Rush _ Know Your Meme.html'];
+	// $offlinePages = ['staticpages/Slender Man _ Know Your Meme.html','staticpages/Forever Alone _ Know Your Meme.html', 'staticpages/Zerg Rush _ Know Your Meme.html'];
 
 	//Regexes for matching values from extractions
 	$rgx_src = '/data-src="(.*)" src/'; //image url
@@ -58,13 +80,15 @@ function mainExecute() {
 	$rgx_views = '/>(.*)</';  //View count
 	$rgx_origin = '/>(.*)</'; //Origin
 	$rgx_org_year = '';
-	
+	$rgx_nsfw = '/>NSFW</';
+
 	//utility variables specifying desired Dom content selectors for extractContent function
 	$meme_img_path = '.entry_list .photo img';
 	$meme_url_path = '.entry_list h2 > a';
 	$meme_faves_path = '.num';
 	$meme_views_path = 'dd.views a';
 	$meme_origin_path = 'dd.entry_origin_link';
+	$meme_nsfw_path = 'span.label-nsfw';
 
 	//Variables to store scraped content
 	$meme_name = '';
@@ -75,16 +99,13 @@ function mainExecute() {
 	$meme_origin = null;
 	$meme_year = null;
 	$meme_learn_more = '';
-	
-	//support variables for logic
-	$matches = [];
-	$alt_dom = '';
-	$i = 1;
-	$memeIDCounter = 1;
-	$page_count = 51;
 
+	//Recreate CSV file
+	createCSV($csvFileName);
+	
 	//main work loop
 	while ($i < $page_count) {
+
 		//counter for tracking meme url index in $meme_href
 		$j = 0;
 
@@ -94,27 +115,28 @@ function mainExecute() {
 		//pull and store scraped dom
 		$html = getDOM('http://knowyourmeme.com/memes/popular/page/' . $i);
 
-
-		// if ($i == 0) { //for first page
-		// 	$html = getDOM('http://knowyourmeme.com/memes/popular');	
-		// 	// $html = getDOM('staticpages/kym_popular_1.html');	
-		// } else {//for other pages up to value i is set to
-		// 	$html = getDOM('http://knowyourmeme.com/memes/popular/page/' . $i);
-		// }
-
 		delay();
 
 		//These arrays should refer to the same memes on the same indexes
 		//extracts array used for meme images and titles
 		$img_content = extractContent($html, $meme_img_path);
-		// $img_content = $html->find($meme_img_path);
+
 		//extracts array used for meme url to access additional content
 		$meme_href = extractContent($html, $meme_url_path);
-		// print_r($img_content);
-		// print_r($meme_href);
 
-		foreach ($img_content as $curr) {			
-			//save meme name
+		foreach ($img_content as $curr) {	
+
+			//check if meme is NSFW
+			$meme_nsfw = extractContent($curr, $meme_nsfw_path);
+
+			if(!empty($meme_nsfw)) {
+				$active = 1;
+			} else {
+				$active = 0;
+			}
+			echo "Meme NSFW: $active <br>";
+
+			// //save meme name
 			$meme_name = getValue($curr, $rgx_title, false, '');
 			preg_match($rgx_title, $curr, $matches);
 			$meme_name = $matches[1];
@@ -126,7 +148,6 @@ function mainExecute() {
 			$meme_img_url = $matches[1];
 			echo "Meme IMG URL: " . $meme_img_url . "<br>";
 
-
 			//get href for current meme in $curr and set variable
 			preg_match($rgx_pg_href, $meme_href[$j], $matches);
 			$meme_learn_more = "http://knowyourmeme.com" . $matches[1];
@@ -137,7 +158,6 @@ function mainExecute() {
 			$alt_dom = getDOM("http://knowyourmeme.com" . $matches[1]);
 			delay();
 			// $alt_dom = getDOM($offlinePages[$m]);
-			echo "Some other MEME URL: " . $matches[1] . "<br>";
 
 			//extract favorite count
 			$meme_faves = getValue($alt_dom, $rgx_faves, true, $meme_faves_path);
@@ -156,7 +176,6 @@ function mainExecute() {
 			saveImg($meme_img_url, $memeIDCounter);
 			$memeIDCounter++;
 
-
 			// //extract meme origin
 			// $origin_segment = extractContent($alt_dom, $meme_origin_path);
 			// preg_match($rgx_origin, $origin_segment[0], $matches);
@@ -165,15 +184,15 @@ function mainExecute() {
 
 			echo "<br>";
 
-
-
 			//create Mog object and call save method
-			$mog = new Mog($meme_name, $meme_img_url, $meme_views, $meme_faves, $meme_learn_more);
-			$mog->save();
+			$mog = new Mog($meme_name, $meme_img_url, $meme_views, $meme_faves, $meme_learn_more, $active);
+			if($toCSV) {
+				$mog->saveToCSV($csvFileName);
+			} else {
+				$mog->saveToDB();	
+			}
 
 			// $m++;
-
-			//Save Mog image for local use
 			
 			$j++;
 		}
@@ -183,7 +202,7 @@ function mainExecute() {
 	echo "all pages done!";
 }
 
-//Support Function: get's DOM at given url
+//Get's DOM at given url
 function getDOM($given) {
 	//set user agent (to avoid 403 errors) and save html from set destination
 	ini_set('user_agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36'); 
@@ -191,12 +210,13 @@ function getDOM($given) {
 	return $html;
 }
 
-//Support Function: returns extraction of content given dom and selector 
+//Returns extraction of content given dom and selector 
 function extractContent($given_html, $selector) {
 	$result = $given_html->find($selector);
 	return $result;
 }
 
+//Gets desired content given arguments
 function getValue($dom, $rgx, $extract, $selector) {
 	if ($extract) {
 		$segment = extractContent($dom, $selector);
@@ -205,10 +225,17 @@ function getValue($dom, $rgx, $extract, $selector) {
 		$segment = $dom;
 		preg_match($rgx, $segment, $matches);
 	}
-	$result = scrubMatch($matches[1]);
+	
+	if (!empty($matches)){
+		$result = scrubMatch($matches[1]);	
+	} else {
+		$result = 0;
+	}
+	
 	return $result;
 }
 
+//scrubs html entities from match
 function scrubMatch($given) {
 	$result = $given;
 	if (substr($given,0,1) == '<') {
@@ -224,6 +251,7 @@ function delay() {
 	sleep(rand($min, $max));
 }
 
+//saves scraped image file
 function saveImg($given, $index) {
 	$ch = curl_init($given);
 	$path = "../../database/img/mogs/$index";
@@ -236,4 +264,9 @@ function saveImg($given, $index) {
 	fclose($fp);
 }
 
-
+//creates csv file for storing scraped information
+function createCSV($filename) {
+	$csv = fopen($filename, "w") or die("Unable to open file!");
+	fwrite($csv, 'name,src_url,rating,active' . PHP_EOL);
+	fclose($csv);
+}
